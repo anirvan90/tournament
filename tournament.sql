9a84886ac83aa63a0e721dc2a26c2dd4653b4638
anirvan90 <anirvan.awatramani@gmail.com>
2015-11-24 17:12:18 +0530

    Update matches_played VIEW, player_wins VIEW and standings VIEW

--- tournament.sql
+++ tournament.sql
@@ -18,24 +18,23 @@ CREATE TABLE matches
   winner INTEGER REFERENCES players(id), 
   loser INTEGER REFERENCES players(id));
 
-/* Create win count view */
-CREATE VIEW win_count AS
-  SELECT id, name, COUNT(matches.winner) AS wins
+/* Create match count view */
+CREATE VIEW matches_played AS
+  SELECT id, name, COUNT(players.id) AS played
   FROM players, matches
-  WHERE players.id = matches.winner
+  WHERE players.id = matches.winner OR players.id = matches.loser
+  GROUP BY id;
+
+/* Create win count view */
+CREATE VIEW player_wins AS
+  SELECT id, name, COUNT (matches.winner) AS wins
+  FROM players, matches
   GROUP BY id
   ORDER BY wins DESC;
 
-/* Create match count view */
-CREATE VIEW matches_played AS
-  SELECT id, COUNT(players.id) AS played
-  FROM players, matches
-  WHERE players.id = matches.winner OR players.id = matches.loser
-  GROUP BY id
-  ORDER BY played DESC;
-
 /* Create standings view */
 CREATE VIEW standings AS
-  SELECT * FROM win_count
-  JOIN matches_played
-  ON win_count.id = matches_played.id
+  SELECT * FROM matches_played
+  LEFT JOIN player_wins
+  ON players.id = matches.winner
+  ORDER BY wins DESC;
